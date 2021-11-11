ce38ff4a1a0e9032ffba4b6161df11139bf2e055
anegassi <89844342+anegassi@users.noreply.github.com>
2021-11-11 06:44:35 -0800

    message

--- lab7b/employeeList.js
+++ lab7b/employeeList.js
@@ -12,7 +12,7 @@ randomFetchedData.then(response=> response.json())  //  parse the response as JS
                        <img src="${emp.picture.medium}" alt="" />
                        <article id="article">
                          <ul>
-                           <li id=""> ${emp.name.first} ${emp.name.last}</li>
+                           <li> ${emp.name.first} ${emp.name.last}</li>
                            <li>phone : ${emp.cell}</li>
                            <li> ${emp.email}</li>
                          </ul>
@@ -32,16 +32,16 @@ randomFetchedData.then(response=> response.json())
                 let arrayOfEmployees= response.results;     
                 console.log(arrayOfEmployees);
                 for(let emp of arrayOfEmployees){
-                   let row=`   <div id="innerDiv" class="innerDiv">
-                      <div id="imgDiv"> <img src="${emp.picture.medium}" alt="" /> </div>
-                       <article id="article">
-                         <ul>
-                           <li id=""> <b>${emp.name.first} ${emp.name.last}</b></li>
-                           <li>phone : ${emp.cell}</li>
-                           <li> ${emp.email}</li>
-                         </ul>
-                       </article>
-                     </div>`
+                  let row=`   <div id="innerDiv" class="innerDiv">
+                  <img src="${emp.picture.medium}" alt="" />
+                  <article id="article">
+                    <ul>
+                      <li> ${emp.name.first} ${emp.name.last}</li>
+                      <li>phone : ${emp.cell}</li>
+                      <li> ${emp.email}</li>
+                    </ul>
+                  </article>
+                </div>`
                      innerDiv.innerHTML+=row;
                 }
                 });
